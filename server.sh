5aa8974b355d79de7d0f5ad9fb0c5c2fd7a976ea
Tommy <TommyCohn116@gmail.com>
2016-06-07 14:50:11 -0400

    Made a bash shell file to make starting the server easier.

---
+++ server.sh
@@ -0,0 +1,7 @@
+cd ./Webpage;
+printf "\n\nGo to the following link:\nfile://";
+link=`pwd`;
+printf "$link";
+printf "/index.html\n\n\nStarting websocket server.\nPress control+c to exit. Do not use control+c to copy!";
+cd ../Server;
+python3 server.py;
