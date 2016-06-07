ab534eec83da7f0db82c4bb9881e8ebafd04df0a
Tommy <TommyCohn116@gmail.com>
2016-06-07 14:45:31 -0400

    Comments and instructions for usage

--- Server/server.py
+++ Server/server.py
@@ -3,15 +3,21 @@
 # Modified from https://websockets.readthedocs.io/en/stable/intro.html
 # Pushes the current utc time to every second on port 12345
 
+# asyncio and websockets are used to open the connection and send and receive things
 import asyncio
-import datetime
 import websockets
 
+# datetime is a package that provides a lot of resources pertaining to time
+# This program uses it to send a UTC timestamp through the socket to the webpage
+import datetime
+
+# This is the section of code that actually sends the message
+# I don't completely understand how this works, but my plan should still work -- I want to send data every tenth of a second or so
 async def time(websocket, path):
     while True:
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        await websocket.send(now)
-        await asyncio.sleep(0.01)
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # Put the message together
+        await websocket.send(now) #Send the message
+        await asyncio.sleep(0.01) #Wait 0.01 seconds
 
 start_server = websockets.serve(time, '127.0.0.1', 12345)
 
